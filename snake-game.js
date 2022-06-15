5158c8f7144fb1e0d9eb1793cf168b5ec90a6f17
Caique Gomes <102303013+caiquegom@users.noreply.github.com>
2022-06-15 16:21:15 -0300

    initial commit
    (PROJETO INCOMPLETO)

---
+++ snake-game.js
@@ -0,0 +1,101 @@
+let stage = document.getElementById("stage");
+let context = stage.getContext('2d');
+
+let snake = {
+    posY: [0],
+    posX: [0],
+    newSquares: 0,
+}
+
+let movement = {
+    x: 0,
+    y: 1,
+}
+
+let scorePoint = {
+    posX: null,
+    posY: null,
+}
+
+function drawSnake(i) {
+    context.fillStyle = 'white'
+    context.fillRect(snake.posX[i],snake.posY[i], 25,25);
+    context.strokeRect(snake.posX[i],snake.posY[i], 25,25);
+    context.lineWidth = 2
+    context.stroke();
+    context.fill();
+}
+
+function drawStage() {
+    context.fillStyle = 'black';
+    context.fillRect(0,0, 500,500);
+    context.fill();
+}
+
+
+function move() {
+    if (snake.newSquares != 0) {
+        for (let index = snake.newSquares; index > 0; index--) {
+            snake.posX[index] = snake.posX[index - 1];
+            snake.posY[index] = snake.posY[index - 1];
+            drawSnake(index);
+        }
+    }
+
+    snake.posX[0] += movement.x * 25;
+    snake.posY[0] += movement.y * 25;
+    drawSnake(0);
+}
+        
+
+function update() {   
+    drawStage();
+    drawScorePoint();
+    move();
+    verifyPoint();
+}
+
+// Detecta a tecla que está sendo pressionada e muda a direção
+document.addEventListener('keydown', function (event) {
+    switch (event.key) {
+        case ('ArrowUp'):
+            movement.x = 0;
+            movement.y = -1;
+            break;
+        case ('ArrowDown'):
+            movement.x = 0;
+            movement.y = 1;
+            break;
+        case ('ArrowRight'):
+            movement.x = 1;
+            movement.y = 0;
+            break;
+        case ('ArrowLeft'):
+            movement.x = -1;
+            movement.y = 0;
+            break;
+    }
+})
+
+function generateScorePointCoordinates() {
+    do{
+        scorePoint.posX = (Math.floor(Math.random() * 20)) * 25;
+        scorePoint.posY = (Math.floor(Math.random() * 20)) * 25;
+    } while(snake.posX.indexOf(scorePoint.posX) != -1 && snake.posY.indexOf(scorePoint.posY) != -1);
+}
+
+function drawScorePoint() {
+    context.fillStyle = '#ff5b5b';
+    context.fillRect(scorePoint.posX,scorePoint.posY, 25,25);
+    context.fill()
+}
+
+generateScorePointCoordinates()
+setInterval(update, 80);
+
+function verifyPoint() {
+    if (snake.posX[0] === scorePoint.posX && snake.posY[0] === scorePoint.posY) {
+        snake.newSquares++;
+        generateScorePointCoordinates()
+    } 
+}
